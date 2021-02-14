da77a3d50f1fc0602f64c44e7dc17e74ec0fe0e8
OlmLisa <36499020+OlmLisa@users.noreply.github.com>
2021-02-14 14:37:56 +0100

    Add files via upload

---
+++ src/com/crunchify/controller/FichierController.java
@@ -0,0 +1,36 @@
+package com.crunchify.controller;
+
+import java.sql.SQLException;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.dao.DevoirDAO;
+
+import com.ecole.Devoir;
+@Controller
+public class FichierController {
+
+    @RequestMapping(value = "/devoir", method = RequestMethod.GET)
+    public ModelAndView showForm() {
+        return new ModelAndView("devoirs", "devoir", new Devoir());
+    }
+ 
+    @RequestMapping(value = "/addDevoir", method = RequestMethod.POST)
+    public String submit(@Valid @ModelAttribute("devoir")Devoir devoir, 
+      BindingResult result, DevoirDAO model) throws SQLException {
+        if (result.hasErrors()) {
+            return "error";
+        }
+        
+        model.setDevoir(devoir);
+        model.enregistrer();
+        return "devoirView";
+    }
+}
